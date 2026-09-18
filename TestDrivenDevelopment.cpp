96c121771e7d1515b48124b39d409c6094a290aa
Manal <manalfaisal48@gmail.com>
2026-09-18 20:07:59 +0500

    Fixed edge cases

--- TestDrivenDevelopment.cpp
+++ TestDrivenDevelopment.cpp
@@ -6,7 +6,7 @@ bool isSorted(const int* arr, const int size){
         return true;
     }
     for (int i = 0 ; i<size-1 ; i++){
-            if (arr[i] < arr[i+1]){
+            if (arr[i] <= arr[i+1]){
                 continue;
             }
             else{
