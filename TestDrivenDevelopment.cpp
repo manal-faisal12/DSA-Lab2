87be8855c25b84a0cf4bc6ff191e210fede841be
Manal <manalfaisal48@gmail.com>
2026-09-18 20:05:11 +0500

    Add initial tests for isSorted function

---
+++ TestDrivenDevelopment.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+bool isSorted(const int* arr, const int size){
+    if (size ==0 || size == 1){
+        return true;
+    }
+    for (int i = 0 ; i<size-1 ; i++){
+            if (arr[i] < arr[i+1]){
+                continue;
+            }
+            else{
+                return false;
+            }
+        }
+    return true;
+}
+void testDuplicateValues(){
+    int arr[] = {3,3,3,3,3};
+    bool result = isSorted(arr, 5);
+    assert(result == true);
+}
+int main(){
+    testDuplicateValues();
+    return 0;
+}
