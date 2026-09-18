d99a6ceb47d08e46461d04476cc7d238124e3440
Manal <manalfaisal48@gmail.com>
2026-09-18 20:07:16 +0500

    Implemented isSorted function

--- TestDrivenDevelopment.cpp
+++ TestDrivenDevelopment.cpp
@@ -20,6 +20,11 @@ void testDuplicateValues(){
     bool result = isSorted(arr, 5);
     assert(result == true);
 }
+void testIncreasing(){
+    int arr[] = {1,2,3,4,5};
+    bool result = isSorted(arr,5);
+    assert(result == true);
+}
 int main(){
     testDuplicateValues();
     return 0;
