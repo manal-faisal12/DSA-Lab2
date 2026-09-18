c73362e7854c234653ba9e332c46c99c186efbd6
Manal <manalfaisal48@gmail.com>
2026-09-18 19:26:38 +0500

    Implement salary program with dynamic allocation

---
+++ salary.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	const int size = 5;
+	int *salArray=new int[size];   
+	for (int i = 0; i < size; i++) {
+		cout << "Enter Salary for employee " << (i + 1) << ": ";
+		 cin >> salArray[i];
+	}
+
+	for (int i = 0; i < size; i++) {
+		salArray[i] = salArray[i] + salArray[i] / (i + 1);
+	}
+
+
+	cout << "\nUpdated Salaries: ";
+	for (int i = 0; i < size; i++) {
+		cout << salArray[i] << " ";
+}
+
+	delete [] salArray; 
+	cout << endl;
+	return 0;
+}
